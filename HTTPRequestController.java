a79102363fc9f3587ead5692bcce2faf45a55059
민병욱 <woook27@naver.com>
2022-11-29 19:29:21 +0900

    feat: HTTPRequest Controller

--- HTTPRequestController.java
+++ HTTPRequestController.java
@@ -1,13 +1,18 @@
 package problem;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class HTTPRequestController {
 	
 	private String host;
 	
 	public HTTPRequestController(String serverURL) {
-		this.host=serverURL;
+		this.host = serverURL;
 	}
 	
 	/*
@@ -18,41 +23,65 @@ public class HTTPRequestController {
 	 */
 	
 	// POST /login API
-	public void setLoginRequest(PrintWriter pw,String requestBody){
+	public void setLoginRequest(PrintWriter pw, String requestBody){
 		pw.println("POST /login HTTP/1.1");
-		pw.println("Host: "+host);
+		pw.println("Host: " + host);
 		pw.println("Content-Type : application/json; charset=utf-8");
-		pw.println("Content-Length :"+requestBody.getBytes().length);
+		pw.println("Content-Length :" + requestBody.getBytes().length);
 		pw.println();
 		pw.println(requestBody);
 		pw.flush();
 	}
 	
-	public void setGetRequest(PrintWriter pw,String requestParam){
+	public void setGetRequest(PrintWriter pw, String requestParam){
 		//TODO : 상품 조회를 요청하는 GET /{학번} API Request를  필수헤더를 포함하여 구성하시오
-
+		pw.println("GET /" +  requestParam + " HTTP/1.1");
+		pw.println("Host: " + host);
+		pw.println();
+		pw.flush();
 	}
 	
 	
-	public void setPostRequest(PrintWriter pw,String requestBody){
+	public void setPostRequest(PrintWriter pw, String requestBody){
 		//TODO : 상품 추가를 요청하는 POST / API Request를 필수헤더를 포함하여 구성하시오
-		
+		pw.println("POST / HTTP/1.1");
+		pw.println("Host: " + host);
+		pw.println("Content-Type : application/json; charset=utf-8");
+		pw.println("Content-Length :" + requestBody.getBytes().length);
+		pw.println();
+		pw.println(requestBody);
+		pw.flush();
 	}
 	
 	
-	public void setPutRequest(PrintWriter pw,String requestBody){
+	public void setPutRequest(PrintWriter pw, String requestBody){
 		// TODO : 상품 수정을 요청하는 PUT / API Request를 필수헤더를 포함하여 구성하시오
-
+		pw.println("PUT / HTTP/1.1");
+		pw.println("Host: " + host);
+		pw.println("Content-Type : application/json; charset=utf-8");
+		pw.println("Content-Length :" + requestBody.getBytes().length);
+		pw.println();
+		pw.println(requestBody);
+		pw.flush();
 	}
 	
-	public void setPatchRequest(PrintWriter pw,String requestBody){
+	public void setPatchRequest(PrintWriter pw, String requestBody){
 		// TODO : 상품 수정을 요청하는  PATCH / API Request를 필수헤더를 포함하여 구성하시오
-
+		pw.println("PATCH / HTTP/1.1");
+		pw.println("Host: " + host);
+		pw.println("Content-Type : application/json; charset=utf-8");
+		pw.println("Content-Length :" + requestBody.getBytes().length);
+		pw.println();
+		pw.println(requestBody);
+		pw.flush();
 	}
 	
 	
 	public void setDeleteRequest(PrintWriter pw,String requestParam){
 		//TODO : 상품 삭제를 요청하는 DELETE /{상품id} API Request를 필수헤더를 포함하여 구성하시오
-
+		pw.println("DELETE / + " +  requestParam + " HTTP/1.1");
+		pw.println("Host: " + host);
+		pw.println();
+		pw.flush();
 	}
 }
